82e585c25f37b28738094a428bdce69242ea1cf1
deimors <deimors@gmail.com>
2019-05-27 20:23:45 -0600

    Attraction

---
+++ Assets/Code/MouseFollower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+	public Transform Target;
+	public Camera Camera;
+
+	void Update()
+	{
+		Target.position = Camera.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
---
+++ Assets/Code/PointEffectorMagnitudeInverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointEffectorMagnitudeInverter : MonoBehaviour
+{
+	public PointEffector2D Effector;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+			Effector.forceMagnitude *= -1;
+	}
+}
